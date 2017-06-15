e38f2781309d27e4122621cbb8e873e9e039db7d
alexander ames <alexanderthomasames@gmail.com>
2017-06-15 15:15:25 -0700

    create app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import React, {Component} from 'react';
 class App extends Component {
 	render() {
 		return (
-			<div><div/>
+			<div>
+				<div className="Title">Music</div>
+				<div>
+					<input placeholder="search here..."/>
+					<button>submit</button>
+				</div>
+				<div className="Profile">
+					<div>Artist Picture</div>
+					<div>Artist</div>
+				</div>
+				<div className="Gallery">
+					Gallery
+				</div>
+			</div>
 		)
 	}
 }
